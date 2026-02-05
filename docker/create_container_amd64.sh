8065a57db1e0d4a11353bd2b55c70bdf381038d1
hhplinh <huynhhaphuonglinh@gmail.com>
2026-02-05 21:31:34 +0900

    fix: update local folder paths in create_container_amd64.sh

--- docker/create_container_amd64.sh
+++ docker/create_container_amd64.sh
@@ -2,9 +2,14 @@
 
 ### EDIT THIS TO WHEREVER YOU'RE STORING YOU DATA ###
 # folder should exist before you mount it
-LOCAL_DATA_FOLDER=/media/jmor6670/T7/datasets
-LOCAL_RESULTS_FOLDER=~/results/
-LOCAL_DYNO_SAM_FOLDER=~/Code/src/DynOSAM/
-LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER=~/Code/src/third_party_dynosam/
+# LOCAL_DATA_FOLDER=data
+# LOCAL_RESULTS_FOLDER=results
+# LOCAL_DYNO_SAM_FOLDER=DynoSAM
+# LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER=extras
+
+LOCAL_DATA_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/data"
+LOCAL_RESULTS_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/results"
+LOCAL_DYNO_SAM_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/DynoSAM"
+LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/extras"
 
 bash create_container_base.sh acfr_rpg/dyno_sam_cuda dyno_sam $LOCAL_DATA_FOLDER $LOCAL_RESULTS_FOLDER $LOCAL_DYNO_SAM_FOLDER $LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER
