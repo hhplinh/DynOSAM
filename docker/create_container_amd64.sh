137d1c4b4f61391d3f833e99bbf69b372b51f963
hhplinh <huynhhaphuonglinh@gmail.com>
2026-02-06 12:01:53 +0900

    fix: remove unnecessary comments and streamline data folder paths in create_container_amd64.sh

--- docker/create_container_amd64.sh
+++ docker/create_container_amd64.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 ### EDIT THIS TO WHEREVER YOU'RE STORING YOU DATA ###
-# folder should exist before you mount it
-# LOCAL_DATA_FOLDER=data
-# LOCAL_RESULTS_FOLDER=results
-# LOCAL_DYNO_SAM_FOLDER=DynoSAM
-# LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER=extras
 
 LOCAL_DATA_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/data"
 LOCAL_RESULTS_FOLDER="/home/linh/Downloads/8_DynOSAM/dynosam_pkg/results"
