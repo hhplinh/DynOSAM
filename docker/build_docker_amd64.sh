4b8378f2db0e70aad98214674be9da4344231195
hhplinh <huynhhaphuonglinh@gmail.com>
2026-02-09 19:55:40 +0900

    fix: update Dockerfile for TensorRT compatibility; adjust data folder paths in scripts

--- docker/build_docker_amd64.sh
+++ docker/build_docker_amd64.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# bash build_docker_base.sh Dockerfile.amd64 acfr_rpg/dyno_sam_cuda
-./build_docker_base.sh Dockerfile.amd64 dynosam:blackwell
+bash build_docker_base.sh Dockerfile.amd64 acfr_rpg/dyno_sam_cuda
+# ./build_docker_base.sh Dockerfile.amd64 dynosam:blackwell
